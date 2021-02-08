55b7101c933a87591523376c49448b3b233cc637
Massashi <massashi21@gmail.com>
2021-02-08 18:04:48 -0300

    project commit

---
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,38 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+  product: Product = {
+    name: "",
+    price: 0
+  }
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readByID(<number><unknown>id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Alterado com sucesso!")
+      this.router.navigate(['/products'])
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
